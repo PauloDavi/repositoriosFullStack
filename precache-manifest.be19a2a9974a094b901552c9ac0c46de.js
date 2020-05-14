b418424be2bdc6a6795271babc9b83b5bacc2c47
Paulo Davi <paulo-davizinho69@gmail.com>
2020-05-14 03:10:35 -0300

    Updates

---
+++ precache-manifest.be19a2a9974a094b901552c9ac0c46de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a41d34bac8e44d45c55be878d8cf881",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "032c42a09f1fe9e264ef",
+    "url": "/react-gh-pages/static/js/2.52aba6d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.52aba6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea807adb798e14e3a94",
+    "url": "/react-gh-pages/static/js/main.cea95158.chunk.js"
+  },
+  {
+    "revision": "55c812bc59dec7a8e735",
+    "url": "/react-gh-pages/static/js/runtime-main.c0cb991e.js"
+  }
+]);
